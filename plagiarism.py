3c0ec8f0a0b68ec84707b996dabf6c91279e5c00
Richard Brinkman <r.brinkman@gmail.com>
2020-06-05 14:53:20 +0200

    Add: conditional formatting

--- plagiarism.py
+++ plagiarism.py
@@ -26,6 +26,20 @@ for column_name in reactions.columns:
         print(df)
         sheet_name = name.replace("[", "").replace("]", "").replace("*", "").replace(":", "").replace("?", "").replace("/", "").replace("\\", "")[:31]
         df.to_excel(writer, sheet_name=sheet_name)
+        rows, columns = df.shape
+        worksheet = writer.sheets[sheet_name]
+        worksheet.conditional_format(1, 1, rows + 1, columns + 1, {
+            'type': '3_color_scale',
+            'min_value': 0.0,
+            'mid_value': 0.5,
+            'max_value': 1.0,
+            'min_type': 'num',
+            'mid_type': 'num',
+            'max_type': 'max',
+            'min_color': '#00FF00',
+            'mid_color': '#FFFF00',
+            'max_color': '#FF0000'
+        })
     except:
         pass
     print()
